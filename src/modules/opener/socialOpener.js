5fd7d2c7dbc6383bba1dd010116dd53754e0bb12
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-07-19 16:44:25 +0600

    improve: Updated opener/socialOpener.js, surfingkeys.bundle.js

--- src/modules/opener/socialOpener.js
+++ src/modules/opener/socialOpener.js
@@ -1,4 +1,4 @@
-api.mapkey("osx", "open twitter @shahjalal_labs", function () {
+api.mapkey("oux", "open twitter @shahjalal_labs", function () {
   if (window.location.hostname.includes("discord")) {
     window.location.href = "https://x.com/shahjalal_labs ";
   } else {
@@ -6,20 +6,20 @@ api.mapkey("osx", "open twitter @shahjalal_labs", function () {
   }
 });
 //t: facebook
-api.mapkey("osf", "Open Facebook", function () {
+api.mapkey("ouf", "Open Facebook", function () {
   window.open("https://www.facebook.com", "_blank");
 });
 
-api.mapkey("ose", "email inbox opening", function () {
+api.mapkey("oue", "email inbox opening", function () {
   window.open("https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox", "_blank");
 });
 
 //t: open whatsApp
-api.mapkey("osw", "whatsApp", function () {
+api.mapkey("ouw", "whatsApp", function () {
   window.open("https://web.whatsapp.com/", "_blank");
 });
 // p: open zoom session
-api.mapkey("osl", "open linkedin", function () {
+api.mapkey("oul", "open linkedin", function () {
   if (window.location.hostname.includes("linkedin")) {
     window.location.href = "https://www.linkedin.com/in/shahjalal-mern/";
   } else {
@@ -27,7 +27,7 @@ api.mapkey("osl", "open linkedin", function () {
   }
 });
 // p: open discord session announcement
-api.mapkey("osd", "open ejp discord sesssion announcement", function () {
+api.mapkey("oud", "open ejp discord sesssion announcement", function () {
   if (window.location.hostname.includes("discord")) {
     window.location.href =
       "https://discord.com/channels/1391805056957026364/1391809512100462592";
--- surfingkeys.bundle.js
+++ surfingkeys.bundle.js
@@ -605,30 +605,30 @@
   });
 
   // src/modules/opener/socialOpener.js
-  api.mapkey("osx", "open twitter @shahjalal_labs", function() {
+  api.mapkey("oux", "open twitter @shahjalal_labs", function() {
     if (window.location.hostname.includes("discord")) {
       window.location.href = "https://x.com/shahjalal_labs ";
     } else {
       window.open("https://x.com/shahjalal_labs ", "_blank");
     }
   });
-  api.mapkey("osf", "Open Facebook", function() {
+  api.mapkey("ouf", "Open Facebook", function() {
     window.open("https://www.facebook.com", "_blank");
   });
-  api.mapkey("ose", "email inbox opening", function() {
+  api.mapkey("oue", "email inbox opening", function() {
     window.open("https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox", "_blank");
   });
-  api.mapkey("osw", "whatsApp", function() {
+  api.mapkey("ouw", "whatsApp", function() {
     window.open("https://web.whatsapp.com/", "_blank");
   });
-  api.mapkey("osl", "open linkedin", function() {
+  api.mapkey("oul", "open linkedin", function() {
     if (window.location.hostname.includes("linkedin")) {
       window.location.href = "https://www.linkedin.com/in/shahjalal-mern/";
     } else {
       window.open("https://www.linkedin.com/in/shahjalal-mern/", "_blank");
     }
   });
-  api.mapkey("osd", "open ejp discord sesssion announcement", function() {
+  api.mapkey("oud", "open ejp discord sesssion announcement", function() {
     if (window.location.hostname.includes("discord")) {
       window.location.href = "https://discord.com/channels/1391805056957026364/1391809512100462592";
     } else {
