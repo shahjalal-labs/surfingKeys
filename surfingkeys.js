697d6b41a2b5817525a371101af66e0efe8d3b58
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-07-29 14:19:29 +0600

    improve: Updated package.json, surfingkeys.js

--- surfingkeys.js
+++ surfingkeys.js
@@ -1,18 +1,30 @@
-import "./src/modules/defaultRemapped";
-import "./src/modules/github";
-import "./src/settings/theme";
-import "./src/settings/settings";
-import "./src/modules/opener/opener";
-import "./src/modules/opener/aiOpener";
-import "./src/modules/opener/mernOpener";
-import "./src/modules/opener/webDevOpener";
-import "./src/modules/opener/socialOpener";
-import "./src/modules/opener/phOpener";
-import "./src/modules/opener/islamicOpener";
-import "./src/modules/yank/imgYank";
-import "./src/modules/markdown/markdown";
-import "./src/modules/hoverClick/hoverClick";
-import "./src/modules/testDate";
-import "./src/modules/fzfFinder";
-import "./src/modules/opener/resourceOpener";
-import "./src/modules/yank/urlYanker";
+import "./src/modules/defaultRemapped.js";
+import "./src/modules/github.js";
+import "./src/settings/theme.js";
+import "./src/settings/settings.js";
+import "./src/modules/opener/opener.js";
+import "./src/modules/opener/aiOpener.js";
+import "./src/modules/opener/mernOpener.js";
+import "./src/modules/opener/webDevOpener.js";
+import "./src/modules/opener/socialOpener.js";
+import "./src/modules/opener/phOpener.js";
+import "./src/modules/opener/islamicOpener.js";
+import "./src/modules/yank/imgYank.js";
+import "./src/modules/markdown/markdown.js";
+import "./src/modules/hoverClick/hoverClick.js";
+import "./src/modules/testDate.js";
+import "./src/modules/fzfFinder.js";
+import "./src/modules/opener/resourceOpener.js";
+import "./src/modules/yank/urlYanker.js";
+
+// sk-56bf47a563244841b72f29af22044533
+//
+settings.defaultLLMProvider = "deepseek";
+
+settings.llm = {
+  deepseek: {
+    apiBaseUrl: "https://api.deepseek.com",
+    apiKey: "sk-56bf47a563244841b72f29af22044533",
+    model: "deepseek-chat",
+  },
+};
