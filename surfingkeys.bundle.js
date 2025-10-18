3510c00e81724850e3d074ffc3e52f71217f573c
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-10-18 09:16:19 +0600

    feat: Added style/chatgpt_prev.js | refactor: Updated style/chatgpt.js, surfingkeys.bundle.js

--- surfingkeys.bundle.js
+++ surfingkeys.bundle.js
@@ -3004,8 +3004,8 @@
     favicon.type = "image/x-icon";
     favicon.rel = "shortcut icon";
     const canvas = document.createElement("canvas");
-    canvas.width = 62;
-    canvas.height = 62;
+    canvas.width = 32;
+    canvas.height = 32;
     const ctx = canvas.getContext("2d");
     ctx.fillStyle = "#561530";
     ctx.beginPath();
