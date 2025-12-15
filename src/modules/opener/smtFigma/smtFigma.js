0f23c9538b9b2b27860027979f3d66b1fd5f40d1
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-12-15 11:46:03 +0600

    refactor: Updated smtFigma/smtFigma.js, surfingkeys.bundle.js

--- src/modules/opener/smtFigma/smtFigma.js
+++ src/modules/opener/smtFigma/smtFigma.js
@@ -1,5 +1,8 @@
 // open visual => ov => visual means figma design which help us to visual view
-api.mapkey("ova", "ashmaster", function () {
+
+//w: (start)╭──────────── ashmaster  ────────────╮
+// open ashmaster
+api.mapkey("ova", "ashmaster figma", function () {
   if (window.location.hostname.includes("www.figma.com")) {
     window.location.href =
       "https://www.figma.com/design/zQdcO8yAzu6RBqaWAHY5Qt/ash_master_Bridge-Network-App?node-id=28320-2244&t=SAlFLJ39wHSWPOjA-1";
@@ -10,3 +13,17 @@ api.mapkey("ova", "ashmaster", function () {
     );
   }
 });
+
+//open ashmaster docs
+api.mapkey("ovda", "ashmaster docs", function () {
+  if (window.location.hostname.includes("docs.google.com")) {
+    window.location.href =
+      "https://docs.google.com/spreadsheets/d/1QUT4qSbz6graO9wLbTLk5rrr37AMKlN09dOIG5dUCTg/edit?gid=0#gid=0";
+  } else {
+    window.open(
+      "https://docs.google.com/spreadsheets/d/1QUT4qSbz6graO9wLbTLk5rrr37AMKlN09dOIG5dUCTg/edit?gid=0#gid=0",
+      "_blank",
+    );
+  }
+});
+//w: (end)  ╰──────────── ashmaster  ────────────╯
--- surfingkeys.bundle.js
+++ surfingkeys.bundle.js
@@ -2753,12 +2753,22 @@
   );
 
   // src/modules/opener/smtFigma/smtFigma.js
-  api.mapkey("ova", "ashmaster", function() {
+  api.mapkey("ova", "ashmaster figma", function() {
     if (window.location.hostname.includes("www.figma.com")) {
       window.location.href = "https://www.figma.com/design/zQdcO8yAzu6RBqaWAHY5Qt/ash_master_Bridge-Network-App?node-id=28320-2244&t=SAlFLJ39wHSWPOjA-1";
     } else {
       window.open(
         "https://www.figma.com/design/zQdcO8yAzu6RBqaWAHY5Qt/ash_master_Bridge-Network-App?node-id=28320-2244&t=SAlFLJ39wHSWPOjA-1",
+        "_blank"
+      );
+    }
+  });
+  api.mapkey("ovda", "ashmaster docs", function() {
+    if (window.location.hostname.includes("docs.google.com")) {
+      window.location.href = "https://docs.google.com/spreadsheets/d/1QUT4qSbz6graO9wLbTLk5rrr37AMKlN09dOIG5dUCTg/edit?gid=0#gid=0";
+    } else {
+      window.open(
+        "https://docs.google.com/spreadsheets/d/1QUT4qSbz6graO9wLbTLk5rrr37AMKlN09dOIG5dUCTg/edit?gid=0#gid=0",
         "_blank"
       );
     }
